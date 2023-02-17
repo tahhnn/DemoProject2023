9f65fb8160aacaacc3a0cd8a806b933130166972
BuiThanh <thanhphuongmao4@gmail.com>
2023-02-17 23:45:35 +0700

    thay doi id

--- XSHOP/admin/model/load1product.php
+++ XSHOP/admin/model/load1product.php
@@ -1,6 +1,6 @@
 <?php
     include 'connect.php';
-    $id = $_GET['id_product'];
+    $id = $_GET['id'];
     $sql_product = "SELECT * FROM products WHERE id_product = $id";
     $statement_product= $connect->prepare($sql_product);
     $statement_product->execute();
